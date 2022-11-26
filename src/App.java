278aad6ad3692c781f0db9df9164cd4569cbadd4
disisVu <ngoanhvu007@gmail.com>
2022-11-26 08:59:42 +0700

    Add createAndShowGUI() to App.java

--- src/App.java
+++ src/App.java
@@ -4,10 +4,19 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class App {
+	
+
+	
+	private static void createAndShowGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame("Dictionary App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		createAndShowGUI();
 	}
 
 }
