a881c082bb1808cc7565c31bd2e1106696ab85c8
disisVu <ngoanhvu007@gmail.com>
2022-11-26 12:26:47 +0700

    Add dict.java

--- src/dict.java
+++ src/dict.java
@@ -1,10 +1,30 @@
+import java.io.*;
 import java.util.*;
 
 public class dict {
+	public static Map<String,String> data = new HashMap<>();
+	
+	public static void importFromFile(String filename) {
+		try {			
+			BufferedReader br = new BufferedReader(new FileReader(filename));			
+			String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] line_data = line.split("`");
+                if (line_data.length > 1) {
+                	data.put(line_data[0], line_data[1]);
+				}
+            }
+			br.close();        
+		}
+		catch (IOException exc) {
+			System.out.println(exc);
+		}
+		
+	}
 
 	public static void main(String[] args) {
-		
-
+		importFromFile(".//src//slang.txt");
+		System.out.println(data);
 	}
 
 }
