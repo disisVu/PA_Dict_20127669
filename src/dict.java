990bf5a71dbb83d337f8a7bd8bc1b237f4c8d563
disisVu <ngoanhvu007@gmail.com>
2022-11-27 09:08:30 +0700

    Update dict.java

--- src/dict.java
+++ src/dict.java
@@ -1,30 +1,48 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class dict {
-	public static Map<String,String> data = new HashMap<>();
+	public static HashMap<String,String[]> data = new HashMap<>();
 	
-	public static void importFromFile(String filename) {
+	// import dictionary data into HashMap
+	public static void importDictionary(String filename) {
 		try {			
-			BufferedReader br = new BufferedReader(new FileReader(filename));			
+			BufferedReader br = new BufferedReader(new FileReader(filename));		
+			
 			String line = "";
             while ((line = br.readLine()) != null) {
+            	
+            	// split into Slang and Definitions
                 String[] line_data = line.split("`");
+                
+                // if Slang goes with Definition
                 if (line_data.length > 1) {
-                	data.put(line_data[0], line_data[1]);
+                	// split Definitions into array
+                	String[] line_value = line_data[1].split(Pattern.quote("|"));
+                	
+                	// store into HashMap
+                	data.put(line_data[0], line_value);
 				}
             }
-			br.close();        
+			br.close();
 		}
 		catch (IOException exc) {
 			System.out.println(exc);
 		}
+	}
+	
+	// search Definition by Slang
+	public static void searchBySlang(String slang) {
 		
 	}
 
 	public static void main(String[] args) {
-		importFromFile(".//src//slang.txt");
-		System.out.println(data);
+		long startTime = System.nanoTime();
+		importDictionary(".//src/slang.txt");
+		long endTime = System.nanoTime();
+		System.out.println(Arrays.toString(data.get("ô")));
+		System.out.println((float)(endTime - startTime) / 1000000000);
 	}
 
 }
